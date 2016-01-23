f99e463d24b8f6abbc24b3a7f00692783d04c42c
drwiner <drwiner@ncsu.edu>
2016-01-23 14:31:23 -0500

    fixed displacement using 2 updates to cell positions

--- Canvas.py
+++ Canvas.py
@@ -1,6 +1,8 @@
-
+from random import random;
+from math import floor
 from Cell import Cell
 from Cell import patternA
+from Cell import coordToOrder
 
 class Canvas(Cell):
     #A Cell has a position, size. Use hasattr to determine if cell is a canvas
@@ -11,44 +13,69 @@ class Canvas(Cell):
         self.genCells();
         self.numRows = int(sqrt(len(self.cellList)));
         self.grandParent = False;
+        self.genPattern();
+        self.pickRandomTarget();
         
     def execute(self):
         return self.cellList;
         #Step 1 - return a list of coordinates and a size to draw
         #Step 2 - narrow down list of coordinates to just those that are some random subset for now, later representing the letter pattern
-
-    #TODO consider a recursive solution: each cell's position depends on changes to one cell (the top left).
-    # Each adjacent cell's location and size will depend on the single adjusted cell
-    # Change to top left --> no position change, just size change. Update adjacent cell positions. If out of bounds, 
-    def updateCells(self,increase):
-        if increase > 0:
-            self.dim += int(increase); # The canvas is now larger.
-            distributedIncrease = int(increase/self.numRows); # The increase divided by the number of cells in the row/col. Thus, increase should be dividible into num rows...
-            self.childCellSize += distributedIncrease; #The children are larger
-            canvasRange = self.getRange();
-            newPositions = (PVector(r,c) for r in canvasRange for c in canvasRange if r%self.childCellSize==0 and c%self.childCellSize ==0)
-            for cell in self.cellList:
-                cell.position = newPositions.next();
-                #print(cell.position);
-                if isinstance(cell,Canvas):
-                    cell.updateCells(int(distributedIncrease));
-                else:
-                    cell.dim = self.childCellSize;
                 
     ### CALLED in INIT ###
     def genCells(self):
         canvasRange = self.getRange();
         # Checked and working: generating range from 0 to 599 (size is 600).
-        
         self.cellList  = [Cell(PVector(r,c),self.childCellSize) for r in canvasRange for c in canvasRange if r%self.childCellSize ==0 and c%(self.childCellSize)==0];
-        for i in self.cellList:
-            print(i.position);
         #Creation of cavnas' children, Not causing error
-        
         return self.cellList;
-
-
-
+    
+    # Each adjacent cell's location and size will depend on the single adjusted cell
+    # Change to top left --> no position change, just size change. Update adjacent cell positions. If out of bounds, 
+    def updateCells(self,increase):
+        if increase > 0:
+            print("center ", self.centerInOrder);
+            self.lastCenterPosition = self.getCenter().position;
+            print("before ", self.lastCenterPosition);
+            #Two phases: first, calculate displacement of center. subtract from origin. second, calculate journey to top left. Divide journey into bouts of 12 by 12. 
+            self.dim += int(increase); # The canvas is now larger.
+            distributedIncrease = int(increase/self.numRows); # The increase divided by the number of cells in the row/col. Thus, increase should be dividible into num rows...
+            self.childCellSize += distributedIncrease; #The children are larger
+            self.updateCellPositions(self.getRange(),distributedIncrease);
+            #calculate displacement
+            print("after ", self.getCenter().position);
+            print("displacement ", self.displacement());
+            self.position.add(self.displacement());
+            self.updateCellPositions(self.getRange(),0);
+            print("after2 ", self.getCenter().position);
+            print("new Position ", self.position);
+            
+    #Returns the cell at index centerInOrder
+    
+    def updateCellPositions(self, canvasRange, distributedIncrease):
+        newPositions = (PVector(r,c) for r in canvasRange for c in canvasRange if (r-self.position.x)%self.childCellSize==0 and (c-self.position.y)%self.childCellSize ==0)
+        for cell in self.cellList:
+            cell.position = newPositions.next(); 
+            #print(cell.position);
+            if isinstance(cell,Canvas):
+                cell.updateCells(int(distributedIncrease));
+            else:
+                cell.dim = self.childCellSize;
+    
+    def getCenter(self):
+        return self.cellList[self.centerInOrder];
+    
+    #Creates a list of integers corresponding to indices in the cellList
+    def genPattern(self):
+        pA = patternA();
+        self.pattern = [coordToOrder(i,12) for i in pA];
+    
+    #Selects a random integer from the pattern, a list of integers
+    def pickRandomTarget(self):
+        spaceSize = len(self.pattern);
+        self.centerInOrder = self.pattern[int(floor(random()*spaceSize))];
+        
+    def displacement(self):
+        return PVector.sub(self.lastCenterPosition, self.getCenter().position);
 
 class CanvasManager:
     #, Canvas(50,0,canvasSize,PVector(0,0))
@@ -79,13 +106,13 @@ class CanvasManager:
             
             #Check if canvas children should become canvi, add them if they are in bounds
             #_____________________________________
-            if canvas.childCellSize >= 144 and not canvas.grandParent:
-                canvas.grandParent = True;
-                for cell in canvas.cellList:
-                    if cellInBounds(cell):
-                        cell = Canvas(cell.position, 12, cell.dim); #This cell is now a canvas. 
-                        self.canvasList.append(cell); #Added to list of active canvi
-                self.canvasList.remove(canvas);
+#             if canvas.childCellSize >= 144 and not canvas.grandParent:
+#                 canvas.grandParent = True;
+#                 for cell in canvas.cellList:
+#                     if cellInBounds(cell):
+#                         cell = Canvas(cell.position, 12, cell.dim); #This cell is now a canvas. 
+#                         self.canvasList.append(cell); #Added to list of active canvi
+#                 self.canvasList.remove(canvas);
                 #BECAUSE, the children of this canvas are canvi themselves... so no longer need to draw them, just the grandchildren.
                 
         return self.canvasList;
@@ -97,16 +124,21 @@ def unpack(pvector):
 #Helpful method for detecting if a cell, be it canvas or not, is on the board
 def cellInBounds(cell, maxCanvasSize = 599):
     x,y = unpack(cell.position);
+    #if top left corner is in
     if inBounds(x) and inBounds(y):
         return True;
-    bx,by = (x+cell.dim-1, y + cell.dim-1);
-    if inBounds(bx) and inBounds(by):
+    #if top right corner is in
+    tx = x+cell.dim-1;
+    if inBounds(tx) and inBounds(y):
         return True;
-    tx,ty = (x+cell.dim-1,y);
-    if inBounds(tx) and inBounds(ty):
+    #if bot left corner is in
+    by = y + cell.dim -1;
+    if inBounds(x) and inBounds(by):
         return True;
-    lx,ly = (x,y+cell.dim-1);
-    if inBounds(lx) and inBounds(ly):
+    #if bot right corner is in
+    if inBounds(tx) and inBounds(by):
+        return True;
+    if x <= 0 and y <= 0:
         return True;
     return False; #Cell not in bounds
 
--- Navon.pyde
+++ Navon.pyde
@@ -4,6 +4,7 @@ from time import sleep;
 from Canvas import unpack;
 from random import random;
 from math import floor
+from Cell import patternA
 
 canvasSize = 600;
 twiceCanvas = canvasSize*2;
@@ -27,7 +28,7 @@ def draw():
 class controlCenter:
     def __init__(self):
         #Create a Canvas Manager by passing an initial Canvas
-        self.canvasManager = CanvasManager(48, canvasSize);
+        self.canvasManager = CanvasManager(50, canvasSize);
         # figure size, growth rate, upperLeftCoord, canvasSize
 
     def execute(self, growing):
@@ -35,15 +36,16 @@ class controlCenter:
         activeCanvi = self.canvasManager.update(growing);
         for i, canvas in enumerate(activeCanvi):
             ## THIS IS USEFUL: print(i);
-            drawCanvas(canvas.execute(),canvas.dim);
+            drawCanvas(canvas.execute(),canvas.dim,canvas.pattern);
             #canvas.execute() returns a list of cells
         #sleep(.1);
         return True;
 
 
 ############ DRAWING METHODS ############
-def drawCanvas(listOfCells,cellSize):
-    testDraw2(listOfCells, cellSize);
+def drawCanvas(listOfCells,cellSize, pattern):
+    #testDraw2(listOfCells, cellSize);
+    patternDraw(listOfCells,cellSize, pattern);
         
 def realDraw(listOfCells):
     fill(0,255,0,191);
@@ -64,7 +66,6 @@ def testDraw2(listOfCells, cellSize):
     colorLevel = 50;
     drawIt = True;
     lastX = 0;
-    print(len(listOfCells));
     for cell in listOfCells:
         cellX,cellY = unpack(cell.position);
         #print(cellX,cellY);
@@ -93,4 +94,19 @@ def randomDraw(listOfCells, cellSize):
             fill(0,255,0,191);
             rect(cellX,cellY,cell.dim,cell.dim);
     
+def patternDraw(listOfCells, cellSize, pattern):
+    textSize(10);
+    colorLevel = 50;
+    for i,cell in enumerate(listOfCells):
+        if i in pattern:
+            cellX,cellY = unpack(cell.position);
+            colorLevel = colorLevel + 2;
+            fill(0,255,0,colorLevel);
+            rect(cellX,cellY,cell.dim,cell.dim);
+            fill(255);
+            text(str(cellX) + ' ' + str(cellY),cellX+2,cellY+10);
+        
+
+        
+
             
